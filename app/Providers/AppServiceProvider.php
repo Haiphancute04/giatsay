106e2063d74d02191071dd0fd67fdd1f172f5890
phanchihai <phanchihai08102004@gmail.com>
2025-12-17 12:38:54 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
@@ -13,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        if($this->app->environment('production')) {
+        URL::forceScheme('https');
+    }
     }
 }
